cdf4934abec9bb8b2da58acef71a0eba95cc16a7
pablokumar1993 <pablokumar1993@gmail.com>
2022-02-10 20:02:47 +0100

    Se ha refactorizado main para hacerlo más escueto y se ha comentado

--- src/Main.java
+++ src/Main.java
@@ -4,53 +4,98 @@ public class Main {
 
         public static void main(String[] args) throws Exception {
 
-            productoReales();
-            productoEnteros();
-            productoRealesDos();
-            productoPotencia();
+            final String SEPARADOR = "***************************************************";
 
+            //Llamadas a procedimientos de la Clase Producto.
+            productoReales(SEPARADOR);
+            productoEnteros(SEPARADOR);
+            productoRealesDos(SEPARADOR);
+            productoPotencia(SEPARADOR);
         }
 
-        public static void productoReales() {
+        /*
+        * CLASE PRODUCTO:
+        */
 
-            double a, b;
+        public static void productoReales(String SEPARADOR) {
+
+            //Variables.
+            double a, b, resultado;
             a = 10;
             b = 5;
 
+            //Creación de Objeto.
             Producto p = new Producto();
-            System.out.println(p.reales(a, b));
+            resultado = p.reales(a, b);
 
+            //Mostrar por la terminal.
+            System.out.println("\n");
+            System.out.println(SEPARADOR);
+            System.out.println("Primer método de Producto (reales).");
+            System.out.println(SEPARADOR);
+            System.out.println("\n");
+            System.out.println(resultado);
         }
-        public static void productoEnteros() {
 
-            int a, b;
+        public static void productoEnteros(String SEPARADOR) {
+
+            //Variables.
+            int a, b, resultado;
             a = 10;
             b = 5;
 
+            //Creación de Objeto.
             Producto p = new Producto();
-            System.out.println(p.enteros(a, b));
+            resultado = p.enteros(a, b);
 
+            //Mostrar por terminal.
+            System.out.println("\n");
+            System.out.println(SEPARADOR);
+            System.out.println("Segundo método de Producto (enteros).");
+            System.out.println(SEPARADOR);
+            System.out.println("\n");
+            System.out.println(resultado);
         }
-        public static void productoRealesDos() {
 
-            double a, b, c;
+        public static void productoRealesDos(String SEPARADOR) {
+
+            //Variables.
+            double a, b, c, resultado;
             a = 3;
             b = 5;
             c = 7;
 
+            //Creación de objeto.
             Producto p = new Producto();
-            System.out.println(p.realesDos(a, b, c));
+            resultado = p.realesDos(a, b, c);
 
+            //Mostrar por terminal.
+            System.out.println("\n");
+            System.out.println(SEPARADOR);
+            System.out.println("Tercer método de Producto (realesDos).");
+            System.out.println(SEPARADOR);
+            System.out.println("\n");
+            System.out.println(resultado);
         }
-        public static void productoPotencia() throws Exception {
 
-            int base, exponente;
+        public static void productoPotencia(String SEPARADOR) throws Exception {
+
+            //Variables.
+            int base, exponente, resultado;
             base = 2;
             exponente = 3;
 
+            //Creación de Objeto.
             Producto p = new Producto();
-            System.out.println(p.potencia(base, exponente));
+            resultado = p.potencia(base, exponente);
 
+            //Mostrar por pantalla.
+            System.out.println("\n");
+            System.out.println(SEPARADOR);
+            System.out.println("Cuarto método de Producto (potencia).");
+            System.out.println(SEPARADOR);
+            System.out.println("\n");
+            System.out.println(resultado);
         }
 
 }
