75bb180cf64b34c9ecef5bdd242f4b2cccd8c74e
pablokumar1993 <pablokumar1993@gmail.com>
2022-02-10 14:13:31 +0100

    Primer Commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Main {
+import grupo7.producto.Producto;
+
+public class Main {
+
+        public static void main(String[] args) throws Exception {
+
+            Producto p = new Producto(); //Una prueba que hice.
+            int potencia = p.potencia(2, 3);
+            System.out.println(potencia);
+
+        }
+
 }
