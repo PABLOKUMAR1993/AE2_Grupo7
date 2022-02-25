2f30ca92841530af7ff8d073f34ff9e97562e9b4
unknown <menendez.david@gmail.com>
2022-02-25 18:09:30 +0100

    iniciado proceso test resta

--- src/grupo7/resta/Resta.java
+++ src/grupo7/resta/Resta.java
@@ -96,5 +96,7 @@ public class Resta {
 		return acumuladoResta;
 	}
 	
+	
+	
 }
 
---
+++ tests/tests/grupo7/resta/TestResta.java
@@ -0,0 +1,28 @@
+package tests.grupo7.resta;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import grupo7.resta.*;
+
+
+class TestResta  {
+
+	Resta resta = new Resta();
+	
+	/**
+	 * En esta prueba unitaria vamos a probar el metodo "restaDosReales"
+	 */	
+	@Test
+	public void restaDosReales (5,7) {
+		
+		
+		
+		//int a =0; // primer número que daremos al método
+		//int b=0; // segundo número que daremos al método
+		int resultadoEsperado =0; // resultado que se espera de este método
+		int resultadoObtenido =0; // resultado obtenido de este método
+		
+	}
+
+}
