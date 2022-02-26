6b15d73dedbbed84e4d57fd331def27680239514
davasda <menendez.david@gmail.com>
2022-02-26 07:25:37 +0100

    Corregido test division

---
+++ tests/tests/grupo7/division/TestCociente.java
@@ -0,0 +1,14 @@
+package test.grupo7.cociente;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCociente {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
